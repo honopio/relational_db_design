0b1d2bf862a232fc95f1447f1f991123929e7317
honopio <honorine.piolat@gmail.com>
2024-03-24 22:14:43 +0100

    add triggers

--- database_create.sql
+++ database_create.sql
@@ -90,7 +90,7 @@ CREATE TABLE Session (
 
 -- Table: Tentative
 CREATE TABLE Tentative (
-    numTentative integer  NOT NULL COMMENT 'Numero de la tentative d''''un etudiant sur un examen. On numerote les tentatives qu''''un etudiant fait sur un examen.',
+    numTentative integer  NOT NULL AUTO_INCREMENT COMMENT 'identifiant unique de la tentative d''''un etudiant sur un examen.',
     date date  NOT NULL COMMENT 'date a laquelle la tentative est faite',
     score integer  NULL COMMENT 'resultat de la tentative, compris entre 0 et 100. attribut pas mandatory car le score n''''est pas connu au moment de l''''enregistrement de la tentative',
     reussi boolean  NULL COMMENT 'Reussi est true si le score est superieur ou egal au scoreMin de l''''examen. On l''''entre comme attribut car on demande une procedure qui marque les tentatives comme reussies.',
@@ -203,7 +203,23 @@ ALTER TABLE Utilisateur_Session ADD CONSTRAINT Utilisateur_Session_Utilisateur F
     ON UPDATE CASCADE
     ON DELETE CASCADE;
 
+
+--------------------TRIGGERS --------------------
+
 DELIMITER //
+-- trigger to check if the Cours begins before it ends
+CREATE TRIGGER dateCours
+BEFORE INSERT ON Cours
+FOR EACH ROW
+BEGIN
+    -- Check if the course start date is before the end date
+    IF NEW.dateDebut >= NEW.dateFin THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Course start date must be before the end date';
+    END IF;
+END //
+
+-- trigger to check if the inscription date is within the course's beginning and end dates
 CREATE TRIGGER dateInscrCours
 BEFORE INSERT ON InscriptionCours
 FOR EACH ROW
@@ -211,12 +227,12 @@ BEGIN
     DECLARE debut DATE;
     DECLARE fin DATE;
 
-    -- Retrieve the corresponding course's start and end dates
+    -- get the course's start and end dates
     SELECT dateDebut, dateFin INTO debut, fin
     FROM Cours
     WHERE numCours = NEW.Cours_numCours;
 
-    -- Check if the inscription date is within the course date range (if the date range is set)
+    -- check if the inscription date is within the dates (if the date range is set)
     IF debut IS NOT NULL AND fin IS NOT NULL THEN
         IF NEW.dateInscription < debut OR NEW.dateInscription > fin THEN
             SIGNAL SQLSTATE '45000'
@@ -224,7 +240,116 @@ BEGIN
         END IF;
     END IF;
 END //
+
+-- trigger to check if the session starts before it ends
+CREATE TRIGGER dateSession
+BEFORE INSERT ON Session
+FOR EACH ROW
+BEGIN
+    IF NEW.dateHeureDebut >= NEW.dateHeureFin THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Session start date must be before the end date';
+    END IF;
+END //
+
+-- trigger to check if the session's date is within the course's beginning and end dates
+CREATE TRIGGER dateSessionCours
+BEFORE INSERT ON Session
+FOR EACH ROW
+BEGIN
+    DECLARE debut DATE;
+    DECLARE fin DATE;
+
+    -- get the course's start and end dates
+    SELECT dateDebut, dateFin INTO debut, fin
+    FROM Cours
+    WHERE numCours = NEW.Cours_numCours;
+
+    -- check if the session date is within the dates (if the date range is set)
+    IF debut IS NOT NULL AND fin IS NOT NULL THEN
+        IF NEW.dateHeureDebut < debut OR NEW.dateHeureDebut > fin THEN
+            SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Session start date must be between course start and end dates';
+        END IF;
+    END IF;
+END //
+
+-- trigger to set the reussi column based on the score
+CREATE TRIGGER tentativeScore
+BEFORE INSERT ON Tentative
+FOR EACH ROW
+BEGIN
+    DECLARE scoreMin INT;
+
+    -- on recupere le score min de l'examen
+    SELECT scoreMin INTO scoreMin
+    FROM Examen
+    WHERE idExamen = NEW.Examen_idExamen;
+
+    -- set la colonne reussi selon le score
+    IF NEW.score >= scoreMin THEN
+        SET NEW.reussi = TRUE;
+    ELSE
+        SET NEW.reussi = FALSE;
+    END IF;
+END //
+
+-- trigger to make sure the user is enrolled in a course before enrolling in a session
+CREATE TRIGGER sessionInscription
+BEFORE INSERT ON Utilisateur_Session
+FOR EACH ROW
+BEGIN
+    DECLARE numCours INT;
+
+    -- get the course number
+    SELECT Cours_numCours INTO numCours
+    FROM Session
+    WHERE numSession = NEW.Session_numSession;
+
+    -- check if the user is enrolled in the course
+    IF NOT EXISTS (
+        SELECT *
+        FROM InscriptionCours
+        WHERE Utilisateur_idUtilisateur = NEW.Utilisateur_idUtilisateur
+        AND Cours_numCours = numCours
+    ) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'User must be enrolled in the course before enrolling in a session';
+    END IF;
+END //
+
+-- trigger to make sure the user is enrolled in a course before taking an exam
+CREATE TRIGGER examInscription
+BEFORE INSERT ON Tentative
+FOR EACH ROW
+BEGIN
+    -- check if the user is enrolled in the course
+    IF NOT EXISTS (
+        SELECT *
+        FROM InscriptionCours
+        WHERE Utilisateur_idUtilisateur = NEW.Utilisateur_idUtilisateur
+        AND Cours_numCours = (
+            SELECT Cours_numCours
+            FROM Partie
+            WHERE numPartie = (
+                SELECT Partie_numPartie
+                FROM Examen
+                WHERE idExamen = NEW.Examen_idExamen
+            )
+        )
+    ) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'User must be enrolled in the course before taking an exam';
+    END IF;
+END //
+
+
+
+    
+
 DELIMITER ;
+
+
 
 -- End of file.
 
--- faker.php
+++ faker.php
@@ -1,17 +1,12 @@
 <?php
 
-
-//First install Faker in your project with the command : composer require fakerphp/faker
-
-
-// when installed via composer
 require_once 'vendor/autoload.php';
 
 // Connect to the database
 $user = 'root';
 $password = '';
 $dbname = 'moocDB';
-$driver = 'mysql';//Change this to your database driver if you use sqlite or postgresql
+$driver = 'mysql';
 
 try {
    $conn = new PDO($driver.':host=localhost;dbname='.$dbname, $user, $password);
@@ -26,7 +21,7 @@ $faker = Faker\Factory::create();
 for ($i = 0; $i < 10; $i++) {
    $intitule = $faker->text(128);
    $description = $faker->realText();
-   $preRequis = $faker->realText(); // only text
+   $preRequis = $faker->realText(); 
 
    //50% chance of generating both dates
     if ($faker->boolean(50)) {
@@ -58,7 +53,6 @@ for ($i = 0; $i < 10; $i++) {
 echo "COURS inserted successfully"."\n";
 
 
-
 /*---------- PARTY TABLE-------------- */
 
 
@@ -67,23 +61,19 @@ $coursStmt = $conn->prepare("SELECT numCours FROM Cours");
 $coursStmt->execute();
 $coursRows = $coursStmt->fetchAll(PDO::FETCH_ASSOC);
 
-// Insert into Partie table for each Cours
 foreach ($coursRows as $coursRow) {
     $Cours_numCours = $coursRow['numCours'];
     
-    //random number of parties, between 1 and 8 per cours
-    //$numParties = $faker->numberBetween(1, 8); // Random number of parties between 1 and 10 per course
-
     // random number of Chapitre, between 1 and 5 per cours
     $howManyChapitre = $faker->numberBetween(1, 5);
     for ($i=1; $i <= $howManyChapitre; $i++) {
         $numChapitre = $i;  //numChapitre auto increments starting from 1
 
-        // random number of parties per chapitre, between 1 and 3 per cours
+        // random number of parties per chapitre, between 1 and 3 per chapitre
         $howManyPartiesPerChapitre = $faker->numberBetween(1, 3);
 
         for ($j=1; $j <= $howManyPartiesPerChapitre; $j++) {
-            $ordreChapitre = $j;  //numPartie auto increments starting from 1
+            $ordreChapitre = $j;  //ordreChapitre auto increments starting from 1
             $titrePartie = $faker->text(128);
             $Contenu = $faker->realText();        
 
@@ -115,7 +105,6 @@ foreach ($partieRows as $partieRow) {
     $numPartie = $partieRow['numPartie'];
     // 50% chance of generating an exam for each Partie
     if ($faker->boolean(50)) {
-        // Generate random exam data
         $titreExamen = $faker->text(30);
         $contenuExamen = $faker->realText();
         /* scoreMin is a multiple of 5 between 40 and 100 */
@@ -136,12 +125,10 @@ echo "EXAMEN inserted successfully"."\n";
 /* ---------- UTILISATEUR TABLE ---------- */
 
 for ($i = 0; $i < 20; $i++) {
-    // Generate random user data
     $nom = $faker->lastName;
     $prenom = $faker->firstName;
     $adresseMail = $faker->email;
 
-    // Insert into Utilisateur table
     $stmt = $conn->prepare("INSERT INTO Utilisateur (nom, prenom, adresseMail) VALUES (:nom, :prenom, :adresseMail)");
     $stmt->bindParam(':nom', $nom, PDO::PARAM_STR);
     $stmt->bindParam(':prenom', $prenom, PDO::PARAM_STR);
@@ -162,7 +149,6 @@ echo "UTILISATEUR inserted successfully"."\n";
     $utilisateurStmt->execute();
     $utilisateurRows = $utilisateurStmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Fetch primary keys from Cours table
     $coursStmt = $conn->prepare("SELECT numCours FROM Cours");
     $coursStmt->execute();
     $coursRows = $coursStmt->fetchAll(PDO::FETCH_ASSOC);
@@ -172,8 +158,8 @@ echo "UTILISATEUR inserted successfully"."\n";
        // Fetch primary key from Utilisateur table
         $Utilisateur_idUtilisateur = $utilisateurRow['idUtilisateur'];
 
-        $usedCourses = array(); // Array to store the courses that have been used
-        //reinitialized for every user
+        // Array to store the courses that have been used, reinitialized for every user
+        $usedCourses = array(); 
 
         $nbInscriptions = $faker->numberBetween(0, 10); // a student is enrolled in a random nb of courses (between 0 and 10)
         for ($j = 1; $j <= $nbInscriptions; $j++) {
@@ -184,7 +170,6 @@ echo "UTILISATEUR inserted successfully"."\n";
             $Cours_numCours = $coursRows[$randomCours]['numCours'];
             } while (in_array($Cours_numCours, $usedCourses)); // Check if the course has been used for this user
             $usedCourses[] = $Cours_numCours; // Add the course to the used courses array
-
             
             /* if the course has dates, dateInscription must be between dateDebut and dateFin */
             $dateCoursStmt = $conn->prepare("SELECT dateDebut, dateFin FROM Cours WHERE numCours = :numCours");
@@ -207,8 +192,8 @@ echo "UTILISATEUR inserted successfully"."\n";
             if ($faker->boolean(50)) {
                 $noteAvis = $faker->numberBetween(0, 5);
 
-                // 80% chance of commentaireAvis not being null if noteAvis is not null
-                if ($faker->boolean(80)) {
+                // 50% chance of commentaireAvis not being null if noteAvis is not null
+                if ($faker->boolean(50)) {
                     $commentaireAvis = $faker->realText();
                 }
             
@@ -229,12 +214,10 @@ echo "INSCRIPTIONCOURS inserted successfully"."\n";
 
 /* ---------- TABLE PROGRESSION ----------------- */
 
-//Fetch Utilisateur_idUtilisateur and Cours_numCours from Utilisateur_Cours table
+//Fetch idUtilisateur and numCours from InscriptionCours table
 $utilisateurCoursStmt = $conn->prepare("SELECT Utilisateur_idUtilisateur, Cours_numCours FROM InscriptionCours");
 $utilisateurCoursStmt->execute();
 $utilisateurCoursRows = $utilisateurCoursStmt->fetchAll(PDO::FETCH_ASSOC); //stores every cours-user pair
-
-//for every user enrolled in a course, insert for every Partie of this course true or false in fini
 
 /* for every cours a user is enrolled in */
 foreach ($utilisateurCoursRows as $utilisateurCoursRow) {
@@ -272,10 +255,7 @@ $reglementStmt->execute();
 $reglementRows = $reglementStmt->fetchAll(PDO::FETCH_ASSOC);
 
 //var_dump($reglementRows);die;
-$num = 1; //variable pour numReglement
 foreach ($reglementRows as $reglementRow) {
-    $numReglement = $num; //demarre a 1
-    $num++;
     $Utilisateur_idUtilisateur = $reglementRow['Utilisateur_idUtilisateur'];
     $Cours_numCours = $reglementRow['Cours_numCours'];
 
@@ -324,7 +304,6 @@ foreach ($coursRowsForSession as $coursRowForSession) {
         //generate a random number of sessions btw 1 and 5
         $nbSessions = $faker->numberBetween(1, 5);
         for ($j = 1; $j <= $nbSessions; $j++) {
-            //$Cours_numCours is the numCours from the Cours table //var_dump($coursRow); die;
             $Cours_numCours = $coursRowForSession['numCours'];
             $faker->boolean(50) ? $capaciteMax = null : $capaciteMax = $faker->numberBetween(5, 100); //50% chance of capaciteMax being null (optional)
             $modalite = $faker->randomElement(['distanciel', 'presentiel']);
@@ -337,33 +316,25 @@ foreach ($coursRowsForSession as $coursRowForSession) {
             $dateCoursStmt->bindParam(':Cours_numCours', $Cours_numCours, PDO::PARAM_INT);
             $dateCoursStmt->execute();
             $dateCoursRows = $dateCoursStmt->fetchAll(PDO::FETCH_ASSOC);
+
+            //if the course has dates, dateHeureDebut and dateHeureFin must be between dateDebut and dateFin
             foreach ($dateCoursRows as $coursRow) {
                 $dateDebut = $coursRow['dateDebut'];
                 $dateFin = $coursRow['dateFin'];
                 if ($dateDebut != null) { //if dates are defined for this course
                     $dateHeureDebut = $faker->dateTimeBetween($dateDebut, $dateFin)->format('Y-m-d H:i:00');
-                }
-                else {
+                    $dateHeureFin = $faker->dateTimeBetween($dateHeureDebut, $dateHeureDebut . '+12 hours')->format('Y-m-d H:i:00');
+                } else {
                     $dateHeureDebut = $faker->dateTimeBetween('now', '+1 year')->format('Y-m-d H:i:00');
+                    $dateHeureFin = $faker->dateTimeBetween($dateHeureDebut, $dateHeureDebut . '+12 hours')->format('Y-m-d H:i:00');
                 }
             }
-            // same for dateHeureFin
-            foreach ($dateCoursRows as $coursRow) {
-                $dateDebut = $coursRow['dateDebut'];
-                $dateFin = $coursRow['dateFin'];
-                if ($dateDebut != null) { //if dates are defined for this course
-                    $dateHeureFin = $faker->dateTimeBetween($dateHeureDebut, $dateHeureDebut . '+12 hours')->format('Y-m-d H:i:00');
-                }
-                else {
-                    $dateHeureFin = $faker->dateTimeBetween($dateHeureDebut, $dateHeureDebut . '+12 hours')->format('Y-m-d H:i:00');
-                }
             $stmt->bindParam(':dateHeureDebut', $dateHeureDebut, PDO::PARAM_STR);
             $stmt->bindParam(':dateHeureFin', $dateHeureFin, PDO::PARAM_STR);
             $stmt->bindParam(':capaciteMax', $capaciteMax, PDO::PARAM_INT);
             $stmt->bindParam(':modalite', $modalite, PDO::PARAM_STR);
             $stmt->bindParam(':Cours_numCours', $Cours_numCours, PDO::PARAM_INT);
             $stmt->execute();
-            }
         }
     }
 }
@@ -380,14 +351,13 @@ $inscriptionCoursStmt = $conn->prepare("SELECT InscriptionCours.Cours_numCours, 
 $inscriptionCoursStmt->execute();
 $inscriptionCoursRows = $inscriptionCoursStmt->fetchAll(PDO::FETCH_ASSOC);
 
-$incrementeur = 1; //variable pour numTentative
-//for every user enrolled in a course with an exam
+//for every user/course pair
 foreach ($inscriptionCoursRows as $inscriptionCoursRow) {
     $Utilisateur_idUtilisateur = $inscriptionCoursRow['Utilisateur_idUtilisateur'];
-
-    //select every examen of the Cours_numCours
+    $Cours_numCours = $inscriptionCoursRow['Cours_numCours'];
+    //select every examen of cours
     $examenStmt = $conn->prepare("SELECT idExamen, scoreMin FROM Examen WHERE Partie_numPartie IN (SELECT numPartie FROM Partie WHERE Cours_numCours = :Cours_numCours)");
-    $examenStmt->bindParam(':Cours_numCours', $inscriptionCoursRow['Cours_numCours'], PDO::PARAM_INT);
+    $examenStmt->bindParam(':Cours_numCours', $Cours_numCours, PDO::PARAM_INT);
     $examenStmt->execute();
     $examenRows = $examenStmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -400,8 +370,6 @@ foreach ($inscriptionCoursRows as $inscriptionCoursRow) {
             $nbTentatives = $faker->numberBetween(1, 3);
             for ($j = 1; $j <= $nbTentatives; $j++) {
                 $Examen_idExamen = $examenRow['idExamen'];
-                $numTentative = $incrementeur; //auto increments starting from 1
-                $incrementeur++;
                 $date = $faker->date();
                 $score = $faker->numberBetween(0, 100);
                 $reussi = $score >= $scoreMin ? true : false; //true if the score is greater or equal to the scoreMin of the examen
@@ -423,12 +391,7 @@ echo "TENTATIVE inserted successfully"."\n";
 
 
 /* ---------- TABLE UTILISATEUR_ROLE ----------------- */
-/* CREATE TABLE Utilisateur_Role (
-    Utilisateur_idUtilisateur integer  NOT NULL COMMENT 'identifiant unique des utilisateurs',
-    Role_idRole integer  NOT NULL COMMENT 'Identifiant unique du role',
-    CONSTRAINT Utilisateur_Role_pk PRIMARY KEY (Utilisateur_idUtilisateur,Role_idRole)
-); */
-//a utilisateur can have more than one role. c'est une table de jointure
+
 //fetch every user
 $utilisateurStmt = $conn->prepare("SELECT idUtilisateur FROM Utilisateur");
 $utilisateurStmt->execute();
@@ -439,14 +402,13 @@ foreach ($utilisateurRows as $utilisateurRow) {
     $Utilisateur_idUtilisateur = $utilisateurRow['idUtilisateur'];
     $nbRoles = $faker->numberBetween(1, 3);
 
-    //a user can't have the same idRole twice
-    //create an array to store the roles that have been used for each user
+    //a user can't have the same idRole twice. create an array to store the roles that have been used for each user
     $usedRoles = array();
 
     for ($j = 1; $j <= $nbRoles; $j++) {
 
         do {
-            $Role_idRole = $faker->numberBetween(1, 5); //random role between 1 and 5
+            $Role_idRole = $faker->numberBetween(1, 5); //random idRole between 1 and 5
         } while (in_array($Role_idRole, $usedRoles)); // Check if the role has been used for this user
         // Add the role to the used roles array
         $usedRoles[] = $Role_idRole ;
@@ -475,11 +437,13 @@ $inscriptionCoursRows2 = $inscriptionCoursStmt2->fetchAll(PDO::FETCH_ASSOC);
 foreach ($inscriptionCoursRows2 as $inscriptionCoursRow2) {
     $Utilisateur_idUtilisateur = $inscriptionCoursRow2['Utilisateur_idUtilisateur'];
     $Cours_numCours = $inscriptionCoursRow2['Cours_numCours'];
+
     //fetch every session of the course
     $sessionStmt = $conn->prepare("SELECT numSession FROM Session WHERE Cours_numCours = :Cours_numCours");
     $sessionStmt->bindParam(':Cours_numCours', $Cours_numCours, PDO::PARAM_INT);
     $sessionStmt->execute();
     $sessionRows = $sessionStmt->fetchAll(PDO::FETCH_ASSOC);
+
     //for every session of the course
     foreach ($sessionRows as $sessionRow) {
         //var_dump($sessionRows);die;
@@ -498,4 +462,4 @@ foreach ($inscriptionCoursRows2 as $inscriptionCoursRow2) {
 }
 echo "UTILISATEUR_SESSION inserted successfully"."\n";
 
-echo "Fake data inserted successfully"."\n";
+echo "\nFake data inserted successfully !"."\n";
