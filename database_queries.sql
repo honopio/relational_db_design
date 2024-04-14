9b19fe7292e535c61078943bf199244319a68f33
Sylvain L <7266910+sylvain-lec@users.noreply.github.com>
2024-04-14 18:24:54 +0200

    better queries for a better world

--- database_queries.sql
+++ database_queries.sql
@@ -1,31 +1,35 @@
 -- 1. Ordonner les cours par popularité
     -- a. par nb d'utilisateurs inscrits
-SELECT Cours_numCours, COUNT(Utilisateur_idUtilisateur) AS numUsersEnrolled
-FROM InscriptionCours
-GROUP BY Cours_numCours
-ORDER BY numUsersEnrolled DESC;
+SELECT C.intitule, COUNT(IC.Utilisateur_idUtilisateur) AS utilisateurs_inscrits
+FROM Cours C
+JOIN InscriptionCours IC ON C.numCours = IC.Cours_numCours
+GROUP BY C.numCours
+ORDER BY utilisateurs_inscrits DESC;
 
     -- b. par notes
-SELECT Cours_numCours, AVG(noteAvis) AS moyenne
-FROM InscriptionCours
-GROUP BY Cours_numCours
-ORDER BY moyenne DESC;
+SELECT C.intitule, AVG(IC.noteAvis) AS note_moyenne
+FROM Cours C
+JOIN InscriptionCours IC ON C.numCours = IC.Cours_numCours
+WHERE IC.noteAvis IS NOT NULL
+GROUP BY C.numCours
+ORDER BY note_moyenne DESC;
 
 -- 2. Pour le cours 3, afficher la liste des utilisateurs :
     -- a. Qui ont terminé le cours (toutes les parties ont été marquées comme validées)
     -- compte le nombre de parties validées pour chaque utilisateur et compare avec le nombre total de parties du cours
-SELECT ic.Utilisateur_idUtilisateur
-FROM InscriptionCours ic
-JOIN Progression p ON ic.Utilisateur_idUtilisateur = p.Utilisateur_idUtilisateur
-JOIN Partie pt ON p.Partie_numPartie = pt.numPartie AND pt.Cours_numCours = 3
-WHERE p.fini = TRUE
-GROUP BY ic.Utilisateur_idUtilisateur
-HAVING COUNT(DISTINCT pt.numPartie) = (SELECT COUNT(numPartie) FROM Partie WHERE Cours_numCours = 3);
+SELECT DISTINCT U.idUtilisateur, U.nom
+FROM Utilisateur U
+JOIN Progression P ON U.idUtilisateur = P.Utilisateur_idUtilisateur
+JOIN Partie Pt ON P.Partie_numPartie = Pt.numPartie
+WHERE Pt.Cours_numCours = 3 AND P.fini = TRUE -- Cours 3 choisi aléatoirement.
+GROUP BY U.idUtilisateur
+HAVING COUNT(Pt.numPartie) = (SELECT COUNT(*) FROM Partie WHERE Cours_numCours = 3);
 
     -- b. Qui ont tenté au moins une fois tous les examens du cours
     -- on select le nb d'examens du cours 3, et on compare pour chaque user au nb de distinct idExamen qu'il a passés pour le cours 3.
-SELECT ic.Utilisateur_idUtilisateur
-FROM InscriptionCours ic
+SELECT DISTINCT U.idUtilisateur, U.nom
+FROM Utilisateur U
+JOIN InscriptionCours ic ON U.idUtilisateur = ic.Utilisateur_idUtilisateur
 JOIN Partie pt ON ic.Cours_numCours = pt.Cours_numCours
 JOIN Examen e ON pt.numPartie = e.Partie_numPartie
 JOIN Tentative t ON ic.Utilisateur_idUtilisateur = t.Utilisateur_idUtilisateur AND t.Examen_idExamen = e.idExamen
@@ -35,8 +39,9 @@ HAVING COUNT(DISTINCT e.idExamen) = (SELECT COUNT(idExamen) FROM Examen WHERE Pa
 
     -- c. Qui ont validés le cours (réussi tous les examens)
     -- on select le nb d'examens du cours 3, et on compare pour chaque user au nb de distinct idExamen qu'il a réussi pour le cours 3 (t.reussi = TRUE).
-SELECT ic.Utilisateur_idUtilisateur
-FROM InscriptionCours ic
+SELECT DISTINCT U.idUtilisateur, U.nom
+FROM Utilisateur U
+JOIN InscriptionCours ic ON U.idUtilisateur = ic.Utilisateur_idUtilisateur
 JOIN Partie pt ON ic.Cours_numCours = pt.Cours_numCours
 JOIN Examen e ON pt.numPartie = e.Partie_numPartie
 JOIN Tentative t ON ic.Utilisateur_idUtilisateur = t.Utilisateur_idUtilisateur AND t.Examen_idExamen = e.idExamen
@@ -54,19 +59,19 @@ GROUP BY r.Utilisateur_idUtilisateur
 ORDER BY montantDepense DESC;
 
 -- 4. Afficher les parties d’un cours, ordonnées par chapitres et ordre dans les chapitres.
-SELECT *
+SELECT numPartie, titrePartie, numChapitre, ordreChapitre
 FROM Partie
-WHERE Cours_numCours = 1
-ORDER BY numChapitre, ordreChapitre;
+WHERE Cours_numCours = 1  -- Cours 1 choisit aléatoirement.
+ORDER BY numChapitre ASC, ordreChapitre ASC;
 
 -- 5. Afficher tous les cours ainsi que les créateurs de cours et formateurs qui y sont rattachés
--- role 2 = créateur, role 3 = formateur
-SELECT c.numCours, u.idUtilisateur, u.nom, u.prenom, ur.Role_idRole 
+SELECT c.numCours, u.idUtilisateur, u.nom, u.prenom, r.nom AS role
 FROM Cours c
 JOIN Cours_utilisateur cu ON c.numCours = cu.Cours_numCours
 JOIN Utilisateur u ON cu.Utilisateur_idUtilisateur = u.idUtilisateur
 JOIN Utilisateur_role ur ON u.idUtilisateur = ur.Utilisateur_idUtilisateur
-WHERE ur.Role_idRole  IN (2, 3);
+JOIN Role r ON ur.Role_idRole = r.idRole
+WHERE ur.Role_idRole IN (2, 3);
 
 -- 6. Pour un utilisateur donné, affiché les cours auxquels il est inscrit, ainsi que son pourcentage de progression de chaque cours 
 -- (nombre de parties marquées comme terminées par rapport au nombre de parties totales du cours)
@@ -90,4 +95,5 @@ SELECT
       WHERE pt.Cours_numCours = ic.Cours_numCours) * 100) AS pourcentageProgression
 FROM InscriptionCours ic
 WHERE ic.Utilisateur_idUtilisateur = 1
-GROUP BY ic.Cours_numCours, ic.Utilisateur_idUtilisateur;
+GROUP BY ic.Cours_numCours, ic.Utilisateur_idUtilisateur
+ORDER BY pourcentageProgression DESC;
